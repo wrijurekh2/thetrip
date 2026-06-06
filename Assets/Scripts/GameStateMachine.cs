80053493b45d885eb253d6ee87753555b0a59992
Ivan Shestakov <108190038+TollGnoll@users.noreply.github.com>
2026-06-06 11:55:10 +0100

    Add logic to switch to score screen

--- Assets/Scripts/GameStateMachine.cs
+++ Assets/Scripts/GameStateMachine.cs
@@ -43,6 +43,7 @@ public class GameStateMachine : MonoBehaviour
         currentState = GameState.Scoring;
         happinessManager.EndDay();
         var scoreCard = scoringSystem.CalculateScoreForDay();
+        notebookManager.SwitchToScoreScreen(scoreCard);
         
     }
 
